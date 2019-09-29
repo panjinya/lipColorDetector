10915e03c17cf7889bab90e42f1c21da51ed7dbc
panjinya <panjinya@126.com>
2019-09-29 16:52:38 +0800

    first commit of some resource and code files to detect lip color

---
+++ getcolor.py
@@ -0,0 +1,21 @@
+import colorsys
+import PIL.Image as Image
+
+#get RGB value
+
+def get_dominant_color(image):
+    max_score = 0.0001
+    dominant_color = None
+    for count,(r,g,b) in image.getcolors(image.size[0]*image.size[1]):
+        # change to HSV 
+        saturation = colorsys.rgb_to_hsv(r/255.0, g/255.0, b/255.0)[1]
+        y = min(abs(r*2104+g*4130+b*802+4096+131072)>>13,235)
+        y = (y-16.0)/(235-16)
+        #ignore high-light
+        if y > 0.9:
+            continue
+        score = (saturation+0.1)*count
+        if score > max_score:
+            max_score = score
+            dominant_color = (r,g,b)
+    return dominant_color
